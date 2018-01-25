04f9d23ed2138ae54bdc2c90ab473e7527c47f87
Darcy <289904316@qq.com>
2018-01-25 23:11:58 +0800

    4-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+
+// import { trigger, state, transition, style, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  /*animations: [
+    trigger('square', [
+      state('green', style({'background-color': 'green', 'height': '100px', 'transform': 'translateY(-100%)'})),
+      state('red', style({'background-color': 'red', 'height': '100px', 'transform': 'translateY(100%)'})),
+      // transition('green => red', animate(1000)),
+      transition('green => red', animate('.5s cubic-bezier(0.68, -0.55, 0.265, 1.55)')),
+      transition('red => green', animate('.5s cubic-bezier(.17,.67,.94,-0.02)'))
+    ])
+  ]*/
 })
 export class AppComponent {
-  title = 'app';
+
+  // squareState: string;
+
+  public darkTheme = false;
+
+  constructor(private oc: OverlayContainer, @Inject('BASE_CONFIG') config) {
+    console.log(config);
+  }
+
+  switchTheme(dark) {
+    this.darkTheme = dark;
+    this.oc.getContainerElement().classList.add(dark ? 'myapp-dark-theme' : null);
+  }
+
+  /*onClick() {
+    this.squareState = this.squareState === 'green' ? 'red' : 'green';
+  }*/
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
-
+import { ProjectModule } from "./project/project.module";
+import { TaskModule } from "./task/task.module";
+import { LoginModule } from "./login/login.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LoginModule,
+    ProjectModule,
+    CoreModule,
+    TaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
